67d33b8146711b0ad7e198374718034e45dc3183
unknown <ken.auth99@gmail.com>
2018-04-26 20:41:53 +0700

    unfinished

---
+++ mission.py
@@ -0,0 +1,55 @@
+# CiiMAV METU
+# Author kenkainkane
+# 26th April 2018
+
+import cv2
+import numpy as np
+import time
+from vision_lib import *
+
+imgName = 'None.jpg'
+count = 0
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.namedWindow('result')
+cap = cv2.VideoCapture(0)
+
+upper = np.array([179, 255, 255])
+lower = np.array([0, 0, 0])
+
+while True :
+    ret, img = cap.read()
+    if img is None :
+        continue
+    r, c, ch = img.shape
+    frame = cv2.resize(img.copy(), (int(c/3), int(r/3)))
+    result = frame
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lower, upper)
+
+    frame = cv2.bitwise_and(frame, frame, mask=mask)
+    frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    ret,thresh = cv2.threshold(mask,127,255,0)
+    frame, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+    for cnt in contours:
+        cv2.drawContours(frame, cnt, -1, (0,255,0), 1)
+        rect = cv2.minAreaRect(cnt)
+        center, wh, angle = cv2.minAreaRect(cnt)
+        x,y = center
+        area = cv2.contourArea(cnt)
+        if area > 2000 :
+            box = cv2.boxPoints(rect)
+            box = np.int0(box)
+            result = cv2.drawContours(frame,[box],0,(0,0,255),2)
+            if count < 1 :
+                imgName = str(time.strftime('%Y_%m_%d_%H_%M'))+'.jpg'
+                cv2.imwrite(imgName, result)
+                count += 1
+    cv2.imshow('result', result)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllwindows()
+cv2.VidepCapture(0).release()
+    
