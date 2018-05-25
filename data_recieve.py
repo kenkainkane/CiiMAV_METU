c93efd07980ee0985c287749b9ddc67c9584cdea
Authapong S <ken.auth99@gmail.com>
2018-05-25 18:23:48 +0700

    Update data_recieve.py

--- data_recieve.py
+++ data_recieve.py
@@ -1,27 +1,19 @@
-print "Start simulator (SITL)"
-import dronekit_sitl
-sitl = dronekit_sitl.start_default()
-connection_string = sitl.connection_string()
-
-# Import DroneKit-Python
 from dronekit import connect, VehicleMode
+import time
+import dronekit_sitl
 
-# Connect to the Vehicle.
-print("Connecting to vehicle on: %s" % (connection_string,))
-vehicle = connect(connection_string, wait_ready=True)
+connection_string = "/dev/ttyACM0"
+baud_rate = 115200
 
-# Get some vehicle attributes (state)
-print "Get some vehicle attribute values:"
-print " GPS: %s" % vehicle.gps_0
-print " Battery: %s" % vehicle.battery
-print " Last Heartbeat: %s" % vehicle.last_heartbeat
-print " Is Armable?: %s" % vehicle.is_armable
-print " System status: %s" % vehicle.system_status.state
-print " Mode: %s" % vehicle.mode.name    # settable
+print(">>>> Connecting with the UAV <<<")
+vehicle = connect(connection_string, baud=baud_rate, wait_ready=True)
 
-# Close vehicle object before exiting script
+vehicle.wait_ready('autopilot_version')
+print('Autopilot version: %s'%vehicle.version)
+print('Supports set attitude from companion: %s'%vehicle.capabilities.set_attitude_target_local_ned)
+
+#- Read the actual position
+print('Position: %s'% vehicle.location.global_relative_frame)
+
 vehicle.close()
-
-# Shut down simulator
-sitl.stop()
-print("Completed")
+print("done")
