431e3e2bd750fa6f6f016f3c470fba14472abf6a
ThongtonN <33448440+NoppanutPat@users.noreply.github.com>
2018-07-05 14:52:05 +0700

    Debug Color_picker.py
    It can adjust a trackbar after click.

--- color_picker.py
+++ color_picker.py
@@ -1,18 +1,23 @@
 import cv2
 import numpy as np
 
+
 colors = []
+
 
 def callback(x):
     pass
+
+
 
 def on_mouse_click (event, x, y, flags, frame):
     if event == cv2.EVENT_LBUTTONUP:
         colors.append(frame[y,x].tolist())
 
+        
+
 def main():
     global colors
-
     capture = cv2.VideoCapture(0)
     cv2.namedWindow('image')
 
@@ -41,6 +46,7 @@ def main():
         ihighS = cv2.getTrackbarPos('highS', 'image')
         ilowV = cv2.getTrackbarPos('lowV','image')
         ihighV = cv2.getTrackbarPos('highV', 'image')
+
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
         if colors:
@@ -51,6 +57,7 @@ def main():
             ihighS = max(c[1] for c in colors)
             ihighV = max(c[2] for c in colors)
 
+   
         lower_hsv = np.array([ilowH, ilowS, ilowV])
         upper_hsv = np.array([ihighH, ihighS, ihighV])
 
@@ -63,16 +70,34 @@ def main():
 
         mask = cv2.inRange(hsv, lower_hsv, upper_hsv)
         frame = cv2.bitwise_and(frame, frame, mask=mask)
+
         cv2.imshow('mask', mask)
         cv2.imshow('frame', hsv)
         cv2.setMouseCallback('frame', on_mouse_click, hsv)
-
+    
         print(lower_hsv, upper_hsv)
+
+        ilowH = lower_hsv[0]
+        ihighH = upper_hsv[0]
+        ilowS = lower_hsv[1]
+        ihighS = upper_hsv[1]
+        ilowV = lower_hsv[2]
+        ihighV = upper_hsv[2]
+
+        cv2.setTrackbarPos('lowH', 'image', ilowH)
+        cv2.setTrackbarPos('highH', 'image', ihighH)
+        cv2.setTrackbarPos('lowS', 'image', ilowS)
+        cv2.setTrackbarPos('highS', 'image', ihighS)
+        cv2.setTrackbarPos('lowV', 'image', ilowV)
+        cv2.setTrackbarPos('highV', 'image', ihighV)
+
+        colors=[]
 
         if cv2.waitKey(1) & 0xFF == ord('q'): #Quit
             break
 
         
+
         if cv2.waitKey(1) & 0xFF == ord('c'): #Reset Trackbar
             ilowH = 0
             ilowS = 0
@@ -80,7 +105,6 @@ def main():
             ihighH = 179
             ihighS = 255
             ihighV = 255
-
             lower_hsv = np.array([ilowH, ilowS, ilowV])
             upper_hsv = np.array([ihighH, ihighS, ihighV])
 
@@ -95,8 +119,14 @@ def main():
 
             continue
 
+
+
     capture.release()
+
     cv2.destroyAllWindows()
 
+
+
 if __name__ == "__main__":
+
     main()
