f1ab3f5a009c1e85cd0726063c00718ca4c1d39a
Authapong S <ken.auth99@gmail.com>
2018-12-15 15:49:50 +0700

    Update mission_lib.py

--- mission_lib.py
+++ mission_lib.py
@@ -3,30 +3,22 @@ import dronekit_sitl
 from serial import *
 import time
 
-#print('Starting Test GOTO...')
-"""
-# DroneKit Variables
-connection_string = "/dev/ttyACM0"
-baud_rate = 115200
-vehicle = connect(connection_string, baud=baud_rate, wait_ready=True)
-vehicle.wait_ready('autopilot_version')
-"""
 def Armed(vehicle):
-    print('........Check Armed........')
-    while True:
-    print(">>>> Change Mode to 'GUIDED' <<<<")
-    vehicle.mode = VehicleMode("GUIDED")		
-    print(">>>> ARMED <<<<")
-    vehicle.armed = True
-    if vehicle.armed:
-    	print(">>>> Already Armed <<<<")
-      	break
-    else:
-	continue
-    time.sleep(2)
+	print('........Check Armed........')
+	while True:
+		print(">>>> Change Mode to 'GUIDED' <<<<")
+    		vehicle.mode = VehicleMode("GUIDED")		
+    		print(">>>> ARMED <<<<")
+    		vehicle.armed = True
+    		if vehicle.armed:
+    			print(">>>> Already Armed <<<<")
+      			break
+    		else:
+			continue
+    		time.sleep(2)
 
 def Takeoff(vehicle,alt):
-  print('>>>>>>>> Take off <<<<<<<<<')
+ 	print('>>>>>>>> Take off <<<<<<<<<')
 	vehicle.simple_takeoff(alt)
 	time.sleep(3)
 	print(">>>> Change mode to 'LOITER' <<<<")
@@ -50,20 +42,20 @@ def Change_Alt(vehicle,alt):
 
 
 def goto(vehicle,lat,lon,alt):
-  print(">>>> Change mode to 'GUIDED' <<<<")
-  vehicle.mode = VehicleMode("GUIDED")
-  a_location = LocationGlobalRelative(lat,lon,alt)
-  print('>>>>>>> Going to  '+str(lat)+','+str(lon)+','+str(alt)+' <<<<<<<<<<')
-  vehicle.simple_goto(a_location)
-  time.sleep(3)
-  print(">>> Change mode to 'LOITER' <<<<")
-  vehicle.mode = VehicleMode("LOITER")
-  print('>>>> Complete GOTO <<<<')
+ 	print(">>>> Change mode to 'GUIDED' <<<<")
+  	vehicle.mode = VehicleMode("GUIDED")
+  	a_location = LocationGlobalRelative(lat,lon,alt)
+  	print('>>>>>>> Going to  '+str(lat)+','+str(lon)+','+str(alt)+' <<<<<<<<<<')
+  	vehicle.simple_goto(a_location)
+  	time.sleep(3)
+  	print(">>> Change mode to 'LOITER' <<<<")
+  	vehicle.mode = VehicleMode("LOITER")
+  	print('>>>> Complete GOTO <<<<')
 
 def land(vehicle):
-  print(">>>> LANDING <<<<")
-  vehicle.mode = VehicleMode("LAND")
-  print(">>>> Landing Complete <<<<")
+  	print(">>>> LANDING <<<<")
+  	vehicle.mode = VehicleMode("LAND")
+  	print(">>>> Landing Complete <<<<")
 
 def get_GPSvalue(vehicle):
 	posdata = str(vehicle.location.global_relative_frame).split(':')
