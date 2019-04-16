339fc26da357246bc95e95baaae5c83398a8120b
Authapong S <ken.auth99@gmail.com>
2019-04-16 20:49:10 +0700

    Update open_camera.py

--- open_camera.py
+++ open_camera.py
@@ -2,15 +2,13 @@ import numpy as np
 import cv2
 import sys
 
-if sys.argv[1] == 'body' :
-    cap = cv2.VideoCapture(0)
-if sys.argv[1] == 'bottom' :
-    cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 
 while(True):
     ret, frame = cap.read()
     r,c,ch = frame.shape
-    frame = cv2.resize(frame.copy(),(int(c/2),int(r/2)))
+    cv2.line(frame,(0,int(r/2)),(int(c),int(r/2)),(255,0,0),3)
+    cv2.line(frame,(int(c/2),0),(int(c/2),int(r)),(255,0,0),3)
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
